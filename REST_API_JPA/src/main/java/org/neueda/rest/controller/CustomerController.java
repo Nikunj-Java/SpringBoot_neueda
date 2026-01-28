56732c629c9d5d9a8d865f2884b31a0cff243944
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-01-28 10:29:31 +0000

    REST JPA CRUD

--- REST_API_JPA/src/main/java/org/neueda/rest/controller/CustomerController.java
+++ REST_API_JPA/src/main/java/org/neueda/rest/controller/CustomerController.java
@@ -3,10 +3,9 @@ package org.neueda.rest.controller;
 import org.neueda.rest.entity.Customer;
 import org.neueda.rest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -14,8 +13,27 @@ public class CustomerController {
     @Autowired
     private CustomerService service;
 
-    @PostMapping
+    @PostMapping("/")
     public Customer save(@RequestBody Customer customer) {
         return service.save(customer);
     }
+    @GetMapping("/")
+    public List<Customer> getAllCustomers(){
+        return  service.getAllCustomers();
+    }
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Integer id){
+        return  service.getCustomerById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteCustomer(@PathVariable Integer id){
+        service.deleteCustomer(id);
+        return "Customer deleted Successfully";
+    }
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Integer id, @RequestBody Customer customer){
+        customer.setId(id);
+        return  service.save(customer);
+    }
+
 }
--- REST_API_JPA/src/main/java/org/neueda/rest/service/CustomerService.java
+++ REST_API_JPA/src/main/java/org/neueda/rest/service/CustomerService.java
@@ -5,6 +5,8 @@ import org.neueda.rest.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
     @Autowired
@@ -13,4 +15,15 @@ public class CustomerService {
     public Customer save(Customer customer) {
         return repo.save(customer);
     }
+    public List<Customer> getAllCustomers(){
+        return  repo.findAll();
+    }
+    public Customer getCustomerById(Integer id){
+        return  repo.findById(id).orElseThrow(()-> new RuntimeException("Customer Not Found"));
+    }
+    public void deleteCustomer(Integer id){
+        repo.deleteById(id);
+    }
+    //update by id--> we will use Save Method only
+
 }
